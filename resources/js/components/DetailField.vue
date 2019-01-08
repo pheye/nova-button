2b39b6032b24e3326f9ad6d593e805a036237e25
Brian Dillingham <bdillingham88@gmail.com>
2019-01-07 20:42:57 -0500

    fixes

--- resources/js/components/DetailField.vue
+++ resources/js/components/DetailField.vue
@@ -1,19 +1,16 @@
 <template>
-    <div class="flex border-b border-40 nova-button-wrapper" v-if="visible">
+    <div class="flex border-b border-40 nova-button-wrapper">
         <div class="w-1/4 py-4">
             <label class="font-normal text-80">{{ field.label }}</label>
         </div>
         <div class="w-3/4 py-4">
-            <nova-button v-bind="$props" @clicked="visible = false">
+            <nova-button v-bind="$props" />
         </div>
     </div>
 </template>
 
 <script>
 export default {
-    props: ['resource', 'resourceName', 'resourceId', 'field'],
-    data: () => ({
-        visible: true
-    })
+    props: ['resource', 'resourceName', 'resourceId', 'field']
 }
 </script>
